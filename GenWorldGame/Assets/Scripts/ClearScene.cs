15ec0f464adf22e203388194f2f54e3e0e7a7f73
hubotty <133901651+hubotty@users.noreply.github.com>
2022-08-06 07:06:25 +0000

    add to #5489

--- GenWorldGame/Assets/Scripts/ClearScene.cs
+++ GenWorldGame/Assets/Scripts/ClearScene.cs
@@ -2,4 +2,18 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ClearScene : MonoBehaviou
+public class ClearScene : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        ClearSceneFromVoxels();
+    }
+
+    //  Clears all voxel clones from the scene 
+    public void ClearSceneFromVoxels()
+    {
+        GameObject[] GameObjects = GameObject.FindGameObjectsWithTag("Voxel");
+
+        for (int i = 0; i < GameObjects.Length; i++)
+       
