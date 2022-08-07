f0ce00c2422bd2543bdd34b16c643687bc34e7b5
hubotty <133901651+hubotty@users.noreply.github.com>
2022-08-07 22:45:47 +0000

    Update auth.py

--- GenWorldGame/Assets/Scripts/VoxelTilePlacer.cs
+++ GenWorldGame/Assets/Scripts/VoxelTilePlacer.cs
@@ -27,4 +27,16 @@ public class VoxelTilePlacer : MonoBehaviour
 
         foreach(VoxelTile tilePrefab in TilePrefabs)
         {
-            tilePrefab.C
+            tilePrefab.CalculateSidesColors();
+        }
+
+        int countBeforeAdding = TilePrefabs.Count;
+        for (int i = 0; i < countBeforeAdding; i++)
+        {
+            VoxelTile clone;
+            switch (TilePrefabs[i].Rotation)
+            {
+                case VoxelTile.RotationType.OnlyRotation:
+                    break;
+
+                case VoxelTile.RotationTyp
