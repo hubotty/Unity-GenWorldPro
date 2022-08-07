75297b39c3b65491c9c22c7e158d8dbd1af39871
hubotty <133901651+hubotty@users.noreply.github.com>
2022-08-07 08:36:18 +0000

    udf: Verify symlink size before loading it

--- GenWorldGame/Assets/Scripts/VoxelTilePlacer.cs
+++ GenWorldGame/Assets/Scripts/VoxelTilePlacer.cs
@@ -7,3 +7,13 @@ using Random = UnityEngine.Random;
 
 public class VoxelTilePlacer : MonoBehaviour
 {
+    //  Time interval between attempts to add new voxels
+    public float waitingTime = 0.1f;
+
+    //  Array for tile prefabs (Links to tiles already placed on the scene)
+    public List<VoxelTile> TilePrefabs;
+
+    //  Variable for the size of our map
+    public Vector2Int MapSize = new Vector2Int(x: 10, y: 10);
+
+    //  Two-dimensional array of tiles (We sav
