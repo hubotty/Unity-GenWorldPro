1b2bb46d58d8d183a08a9dbc621bcd7d5f508c7a
hubotty <133901651+hubotty@users.noreply.github.com>
2022-09-06 16:14:21 +0000

    Regenerate gemspec for version 1.0.3

--- GenWorldGame/Assets/Scripts/VoxelTilePlacerWfc.cs
+++ GenWorldGame/Assets/Scripts/VoxelTilePlacerWfc.cs
@@ -1,2 +1,14 @@
 ﻿using System;
-using System.C
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class VoxelTilePlacerWfc : MonoBehaviour
+{
+    public List<VoxelTile> TilePrefabs;
+    public Vector2Int MapSize = new Vector2Int(10, 10);
+
+    private VoxelTile[,] spawnedTiles;
+
+    private Queue<Vector2Int> recalcPossibleTilesQueue = new Queue<Vector2Int>(
